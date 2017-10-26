a849eea96020be53b2f563dc2538d360dcc3bc85
Elovenkova Maria <32039134+Elovenkova@users.noreply.github.com>
2017-10-26 12:52:48 +0300

    Add files via upload

--- 3.6.c
+++ 3.6.c
@@ -2,64 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 int main()
 {
-    int i, n, K_x, K_y, M_x, M_y, N_x, N_y, L_x, L_y;
-    float d1, d2, cosa, sina, S, d1_x, d2_x, d1_y, d2_y;
-    scanf ("%d", &n);
-    int array[n];
-    for(i=0; i<n; i++)
+    int i;
+    int A[4], B[4];
+    for(i = 0; i < 4; i++)
     {
-        scanf("%d", &array[i]);
+        scanf("%d %d", &A[i], &B[i]);
     }
-    K_x = array[0];
-    K_y = array[1];
-    for(i=0; i<n; i+2)
+    double a, b, c, S;
+    a = pow(pow(A[0]-A[1], 2) + pow(B[0]-B[1], 2), 0.5);
+    b = pow(pow(A[2]-A[1], 2) + pow(B[2]-B[1], 2), 0.5);
+    c = pow(pow(A[0]-A[2], 2) + pow(B[0]-B[2], 2), 0.5);
+    if (c > a && c > b)
     {
-        if (array[i]>=K_x)
-        {
-            K_x=array[i];
-            K_y=array[i+1];
-        }
+        S = a*b;
+        printf("%lf", S);
     }
-    L_x = array[0];
-    L_y = array[1];
-    for(i=0; i<n; i+2)
+    if (a > b && a > c)
     {
-        if (array[i]<=L_x)
-        {
-            L_x=array[i];
-            L_y=array[i+1];
-        }
+        S = b*c;
+        printf("%lf", S);
     }
-    M_y = array[1];
-    M_x = array[0];
-    for(i=1; i<n; i+2)
+    if (b > a && b > c)
     {
-        if (array[i]>=M_y)
-        {
-            M_x=array[i-1];
-            M_y=array[i];
-        }
+        S = a*c;
+        printf("%lf", S);
     }
-    N_x = array[0];
-    N_y = array[1];
-    for(i=1; i<n; i+2)
-    {
-        if (array[i]<=N_y)
-        {
-            N_x=array[i-1];
-            N_y=array[i];
-        }
-    }
-    d1_x = M_x - N_x;
-    d1_y = M_y - N_y;
-    d2_x = K_x - L_x;
-    d2_y = K_y - L_y;
-    d1 = sqrt((powf(d1_x, 2)) + (powf(d1_y, 2)));
-    d2 = sqrt((powf(d2_x, 2)) + (powf(d2_y, 2)));
-    cosa = ((d1_x*d2_x)+(d1_y*d2_y))/(d1*d2);
-    sina = sqrt(1-(powf(cosa,2)));
-    S = (d1*d2*sina)/2;
-    printf("%d",S);
 }
+
