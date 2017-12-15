6a66560647c650c5a5a650bd5275954cf6084f1c
Elovenkova Maria <32039134+Elovenkova@users.noreply.github.com>
2017-12-15 13:42:32 +0300

    Add files via upload

--- 3.8.c
+++ 3.8.c
@@ -1,22 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 int main()
 {
-    double a, b;
+    double a, b, delta;
     scanf ("%lf", &a);
     scanf ("%lf", &b);
+    delta = tan(b) - b - tan(a) + a;
     while ((b-a) > 0.0000000001)
     {
-        if (tan((b+a)/2) - ((b+a)/2)>0)
+        if (delta >= 0)
         {
-            b = (b+a)/2;
-        }
-        else if (tan((b+a)/2) - ((b+a)/2)<0)
-        {
-            a = (b+a)/2;
+            if (tan((b+a)/2) - ((b+a)/2)>0)
+            {
+                b = (b+a)/2;
+            }
+            else if (tan((b+a)/2) - ((b+a)/2)<0)
+            {
+                a = (b+a)/2;
+            }
+            else
+            {
+                printf("%f", (b+a)/2);
+                break;
+            }
         }
         else
         {
-            printf("%f", (b+a)/2);
-            break;
+            if (tan((b+a)/2) - ((b+a)/2)>0)
+            {
+                a = (b+a)/2;
+            }
+            else if (tan((b+a)/2) - ((b+a)/2)<0)
+            {
+                b = (b+a)/2;
+            }
+            else
+            {
+                printf("%f", (b+a)/2);
+                break;
+            }
         }
     }
     printf("%f", (b+a)/2);
